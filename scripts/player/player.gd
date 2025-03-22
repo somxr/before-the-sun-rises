e7582f7e3b7d932896e8c3ff7557e5fcf1ef91de
Somar Jaafar <somar.jaafar19@gmail.com>
2025-03-21 22:47:26 -0400

    initial commit

---
+++ scripts/player/player.gd
@@ -0,0 +1,58 @@
+extends CharacterBody3D
+
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 4.5
+
+var walking = false
+
+@onready var animation_player: AnimationPlayer = $visuals/playerModel/AnimationPlayer
+@onready var visuals: Node3D = $visuals
+
+
+func _ready() -> void:
+	print("we do get ready")
+	#set the duration of the blend time when going from the idle animation to walking animation
+	animation_player.set_blend_time("idle", "walk", 0.2)
+	animation_player.set_blend_time("walk","idle", 0.2)
+
+	
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	#WALKING LOGIC
+	#when a direction is detected you can start walking
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		
+		visuals.look_at(direction+position)
+		
+		#if the state was not already "walking" when a direction is input, 
+		#then change state to walking
+		if !walking:
+			walking = true
+			animation_player.play("walk")
+			
+	else: 
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+		if walking:
+			walking = false
+			animation_player.play("idle")
+
+
+	move_and_slide()
